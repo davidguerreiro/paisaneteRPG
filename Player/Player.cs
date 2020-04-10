4eb80a6179c275bed10eb68a3277c4aaef70d2e7
davidguerreiro <me@davidguerreiro.com>
2020-04-10 17:49:55 +0100

    create scriptable object for items

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Character {
-    
+
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D( Collider2D other ) {
+        
+        // check if the player is colliding with a consumable.
+        if ( other.gameObject.CompareTag( "Collectible" )  ) {
+            other.gameObject.SetActive( false );
+        }
+    }
 }
---
+++ ScriptableObject/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "Item") ]
+
+public class Item : ScriptableObject {
+    public string objectName;                                   // Item name.
+    public Sprite sprite;                                       // Object base sprite.
+    public int quantity;                                        // Amount of this items which is collected.
+    public bool stackable;                                      // Wheter this item will be auto-consumed or saved in the inventory.
+    public enum ItemType {
+        COIN,
+        HEALTH
+    };
+
+    public ItemType itemType;                                   // Type of item, or item class.
+}
