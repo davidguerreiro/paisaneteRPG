78c2da2668d9dd365b2899d8ec0a321dc53135c5
davidguerreiro <me@davidguerreiro.com>
2020-04-25 14:50:20 +0100

    update character script to use the new scriptable object HitPoints

--- MonoBehaviours/Character.cs
+++ MonoBehaviours/Character.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 // abstract class means it cannot be instantiated, and therefore must be inherited by a subclass.
 public abstract class Character : MonoBehaviour {
 
-    public int hitPoints;                                   // Current hit points value.
-    public int maxHitPoints;                                // Maximun hit points value.
+    public HitPoints hitPoints;                                     // Current hit points value.
+    public float maxHitPoints;                                      // Maximun hit points value.
+    public float startingHitPoints;                                 // Number of hit points the character starts with.
 
     // Start is called before the first frame update
     void Start() {
