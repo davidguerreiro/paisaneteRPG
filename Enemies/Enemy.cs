d2c72ef2b4ac489fd44316d9cc7fbbe2ac386046
davidguerreiro <me@davidguerreiro.com>
2020-05-09 18:21:21 +0100

    add colisions logic to enemies to damage player and defeat player

--- Enemies/Enemy.cs
+++ Enemies/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : Character {
 
     float hitPoints;                            // Enemy current hit points. 
+    public int damageStrenght;                  // How much damage the enemy causes to the player.
+    Coroutine damageCoroutine;                  // Reference to damage character coroutine.
 
     /// <summary>
     /// This function is called when the object becomes enabled and active.
@@ -48,5 +50,42 @@ public class Enemy : Character {
         this.hitPoints = startingHitPoints;
     }
 
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D( Collision2D collision ) {
+        
+        // check if the enemy is colliding the player.
+        if ( collision.gameObject.CompareTag( "Player" ) ) {
+            
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            // init damage player.
+            if ( damageCoroutine == null ) {
+                damageCoroutine = StartCoroutine( player.DamageCharacter( damageStrenght, 1.0f ) );
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Sent when a collider on another object stops touching this
+    /// object's collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionExit2D( Collision2D collision ) {
+        
+        // check if the enemy stops colliding with the player.
+        if ( collision.gameObject.CompareTag( "Player" ) ) {
+
+            // check if the enemy was damaging the player before.
+            if ( damageCoroutine != null ) {
+                StopCoroutine( damageCoroutine );
+                damageCoroutine = null;
+            }
+        } 
+    }
 
 }
