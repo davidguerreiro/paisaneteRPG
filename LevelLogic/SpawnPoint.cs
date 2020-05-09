d24c0f3a1b40788fe3385968557d83f3c9c7bb75
davidguerreiro <me@davidguerreiro.com>
2020-05-09 15:02:54 +0100

    fix enemy spawn point

--- LevelLogic/SpawnPoint.cs
+++ LevelLogic/SpawnPoint.cs
@@ -9,7 +9,7 @@ public class SpawnPoint : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        
+        SpawnByInterval();
     }
 
     /// <summary>
