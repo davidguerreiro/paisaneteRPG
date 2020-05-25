7dfc09576bdd22a2ffead7c4cb495d3bb63d44ae
davidguerreiro <me@davidguerreiro.com>
2020-05-25 18:14:58 +0100

    add spaw ammo method to weapon class

--- Player/AttackMechanics/Weapon.cs
+++ Player/AttackMechanics/Weapon.cs
@@ -16,6 +16,56 @@ public class Weapon : MonoBehaviour {
     }
 
     /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update() {
+        
+        if ( Input.GetMouseButtonDown(0) ) {
+            FireAmmo();
+        }
+    }
+
+    /// <summary>
+    /// Spawn ammo method.
+    /// Spawn a new ammo proyectile by
+    /// location.
+    /// </summary>
+    /// <parma name="location">Vector3 - where to spawn the ammo</param>
+    /// <returns>GameObject</returns>
+    private GameObject SpawnAmmo( Vector3 location ) {
+        
+        // loop through the ammo pool.
+        foreach ( GameObject ammo in ammoPool ) {
+
+            // if an unused disabled ammo gameobject is found in the pool, the use it.
+            if ( ammo.activeSelf == false ) {
+
+                ammo.SetActive( true );
+                ammo.transform.position = location;
+
+                return ammo;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Fire ammo when the 
+    /// player press attack button.
+    /// </summary>
+    private void FireAmmo() {
+
+    }
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy() {
+        ammoPool = null;
+    }
+
+    /// <summary>
     /// Set up ammo object pool.
     /// This is done to reuse ammo gameObjects
     /// and improve performance avoinding
@@ -28,7 +78,7 @@ public class Weapon : MonoBehaviour {
             ammoPool = new List<GameObject>();
         }
 
-        for ( int i = 0; i < poolSize; i++ ) {
+        for ( int i = 0; i < this.poolSize; i++ ) {
             GameObject ammoObject = Instantiate( ammoPrefab );
             ammoObject.SetActive( false );
             ammoPool.Add( ammoObject );
