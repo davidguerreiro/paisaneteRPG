47f66dcf04741244abbe7dcbf9bf9ac7dc6f0c2b
davidguerreiro <me@davidguerreiro.com>
2020-05-25 17:48:16 +0100

    add ammo class

---
+++ Player/AttackMechanics/Ammo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour {
+
+    public int damageInflicted;                             // Damage caused by this type of ammo.
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="collision">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D( Collider2D collision ) {
+        
+        // check if enemy has been impacted by proyectile.
+        if ( collision.gameObject.CompareTag( "Enemy" ) && collision is BoxCollider2D ) {
+
+            // damage enemy.
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            StartCoroutine( enemy.DamageCharacter( damageInflicted, 0.0f ) );
+
+            // remove proyectile.
+            gameObject.SetActive( false );
+        }
+    }
+}
